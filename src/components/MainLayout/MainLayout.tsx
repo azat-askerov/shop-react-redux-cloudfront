80d6d6f6a5b5b1b44596f8d576f32c334edc9d27
Azat Askerov <azat.askerov@tvh.com>
2022-07-11 00:23:28 +0600

    task 2: Automate the deployment to the S3 phase

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        This store is created for second task
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
